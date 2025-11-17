a1824a4123a7db6a46db17fa21286ba2aabffcb6
Vinicius <jorgevinicius2002@hotmail.com>
2025-11-17 14:33:33 -0300

    feat: scrollbar, second sidebar content

--- app/components/Sidebar.vue
+++ app/components/Sidebar.vue
@@ -143,11 +143,24 @@ const { setOpen } = useSidebar();
           <SidebarTrigger />
         </div>
       </SidebarHeader>
-      <SidebarContent>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur,
-        sint eveniet? Incidunt porro natus laudantium fuga ratione deleniti sint
-        eius quasi dolorem labore. Quo laborum laudantium reprehenderit
-        aspernatur repellendus fugiat!
+      <SidebarContent class="p-4">
+        <h2 class="text-xl font-medium">Inspire-se</h2>
+        <small class="text-xs text-muted-foreground">
+          17 de novembro de 2025
+        </small>
+        <ul class="space-y-2">
+          <li v-for="_ in 10" class="relative max-h-24 overflow-hidden">
+            <img
+              src="https://i.pinimg.com/736x/d0/de/24/d0de24af870e21f8b3d92c799e1955ed.jpg"
+              alt="Carrosel White and Black"
+              class="object-cover object-center opacity-75"
+            />
+            <div class="absolute bottom-2 inset-x-2">
+              <small>Fotografia e desenhos</small>
+              <h3>Preto e branco aesthetic</h3>
+            </div>
+          </li>
+        </ul>
       </SidebarContent>
     </Sidebar>
   </Sidebar>
